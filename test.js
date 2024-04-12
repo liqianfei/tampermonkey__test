82e3698e0cdf88ee3c1bd97bd9d34b1924fc30a1
liqianfei <849401440@qq.com>
2024-04-12 21:10:41 +0800

    test2

--- test.js
+++ test.js
@@ -1,2 +1,18 @@
-console.log('test')
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-04-12
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @license     AGPL-3.0-or-later
+// ==/UserScript==
 
+;(function () {
+  'use strict'
+  console.log('Hello World!')
+  console.log('Hello World!2')
+  // Your code here...
+})()
